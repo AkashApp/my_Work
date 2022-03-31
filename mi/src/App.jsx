140905ac53fe8acecf2e187d411e8941656483eb
Akash Borate <27akashborate@gmail.com>
2022-03-31 13:06:38 +0530

    commit

--- mi/src/App.jsx
+++ mi/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { Router } from './components/Routers/Router'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Router/>
     </div>
   )
 }
