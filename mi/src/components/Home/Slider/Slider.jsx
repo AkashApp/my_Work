df5f4ecea6c95c19b877700824a3c64c51ae6988
Akash Borate <27akashborate@gmail.com>
2022-04-02 10:11:18 +0530

    commit

---
+++ mi/src/components/Home/Slider/Slider.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+
+export const Slider = () => {
+  return (
+    <Carousel variant="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i01.appmifile.com/webfile/globalimg/0320/TO-B/New-Product-Banner/RN-11Pro-Plus5G-en-Banner.jpg"
+          alt="First slide"
+        />
+        {/* <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i01.appmifile.com/webfile/globalimg/dongxuechun/12propcbanneren.jpg"
+          alt="Second slide"
+        />
+        {/* <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i01.appmifile.com/webfile/globalimg/dongxuechun/rn11pro5g.jpg"
+          alt="Third slide"
+        />
+        {/* <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i01.appmifile.com/webfile/globalimg/0320/TO-B/event/youtube-event-banner.jpg"
+          alt="Third slide"
+        />
+        {/* <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i01.appmifile.com/webfile/globalimg/0320/TO-B/New-Product-Banner/mi-11t-pro-banner.jpg"
+          alt="Third slide"
+        />
+        {/* <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i01.appmifile.com/webfile/globalimg/dongxuechun/k9dbannerEn.jpg"
+          alt="Third slide"
+        />
+        {/* <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+    </Carousel>
+  );
+};
